153d4617ce3a8f7787ac01d2b580b558a7e14b00
itaysegev1 <itaysegev1234@gmail.com>
2024-12-06 12:11:23 +0200

    x

--- client.py
+++ client.py
@@ -107,7 +107,7 @@ if __name__ == "__main__":
     expr_2 = add_b(max_f(2, 3), 3)  # (2)
 
     # (3) '3 + ((4 * 2) / ((1 - 5) ** (2 ** 3)))' = 3.0001220703125
-    # expr = add_b(3, div_b(mul_b(4, 2), pow_b(sub_b(1, 5), pow_b(2, 3)))) # (3)
+    expr3 = add_b(3, div_b(mul_b(4, 2), pow_b(sub_b(1, 5), pow_b(2, 3)))) # (3)
 
     # (4) '((1 + 2) ** (3 * 4)) / (5 * 6)' = 17714.7
     # expr = div_b(pow_b(add_b(1, 2), mul_b(3, 4)), mul_b(5, 6)) # (4)
@@ -121,7 +121,7 @@ if __name__ == "__main__":
 
     # Change the following values according to your needs:
 
-    show_steps = False  # Request the steps of the calculation
+    show_steps =True  # Request the steps of the calculation
     cache_result = True  # Request to cache the result of the calculation
     # If the result is cached, this is the maximum age of the cached response
     # that the client is willing to accept (in seconds)
@@ -130,8 +130,10 @@ if __name__ == "__main__":
     # * Change in start (2)
     client((host, port), expr, show_steps,
            cache_result, cache_control)
+    client((host, port), expr_2, show_steps,
+           cache_result, cache_control)
+    client((host, port), expr3, show_steps,
+                cache_result, cache_control)
     client((host, port), expr, show_steps,
            cache_result, cache_control)
-    client((host, port), expr_2, show_steps,
-                cache_result, cache_control)
     # * Change in end (2)
